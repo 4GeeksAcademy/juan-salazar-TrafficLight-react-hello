69f9af967e5907ec1115fc2e3483ac1e1641bcb5
JRSalazar2598 <155923860+JRSalazar2598@users.noreply.github.com>
2025-03-01 17:42:35 -0600

    traffic-light.jsx

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import '/workspaces/juan-salazar-TrafficLight-react-hello/src/styles/index.css';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const[rojo,setRojo]=useState("secondary")
+	const[amarillo,setAmarillo]=useState("secondary")
+	const[verde,setVerde]=useState("secondary")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [brilloRojo, setBrilloRojo] = useState(false);
+  const [brilloAmarillo, setBrilloAmarillo] = useState(false);
+  const [brilloVerde, setBrilloVerde] = useState(false);
+
+  const prenderRojo=()=>{
+		setRojo("danger")
+		setAmarillo("secondary")
+		setVerde("secondary")
+		setBrilloRojo(true);
+        setBrilloAmarillo(false);
+        setBrilloVerde(false);
+
+		
+	}
+	const prenderAmarillo=()=>{
+		  setAmarillo("warning")
+		  setRojo("secondary")
+		  setVerde("secondary")
+		  setBrilloRojo(false);
+		  setBrilloAmarillo(true);
+		  setBrilloVerde(false);
+	}
+	const prenderVerde=()=>{
+		  setAmarillo("secondary")
+		  setRojo("secondary")
+		  setVerde("success")
+		  setBrilloRojo(false);
+          setBrilloAmarillo(false);
+          setBrilloVerde(true);
+	}
+	return (
+		
+		 <div className="text-center container " style={{ backgroundColor: "black" ,width:"10%",borderRadius: "10px" }}>
+           <div className={`bg-${rojo} rounded-circle mt-1 ${brilloRojo ? 'bright' : ''} `} style={{width:"100px",height:"100px"}} onClick={prenderRojo}>
+			</div> 
+			<div className={`bg-${amarillo} rounded-circle mt-1${brilloAmarillo ? 'bright' : ''}`} style={{width:"100px",height:"100px"}} onClick={prenderAmarillo}>
+			</div>
+			<div className={`bg-${verde} rounded-circle mt-1 ${brilloVerde ? 'bright' : ''}`} style={{width:"100px",height:"100px"}} onClick={prenderVerde}>
+			</div>
 		</div>
 	);
 };
